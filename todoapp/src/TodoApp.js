f1bb5f7aa710ea4a645a269b38a246caabf602f9
GohanBlanco <livio.chantung@gmail.com>
2019-02-04 15:19:24 +0100

    bonjour le monde

---
+++ todoapp/src/TodoApp.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class TodoApp extends Component {
+  render() {
+    return (
+        <div>
+          <ul>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+          </ul>
+
+        </div>
+    );
+  }
+}
+
+export default TodoApp;
